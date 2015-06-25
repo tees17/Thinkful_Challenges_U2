3601730df9aea0a11de3ad23147b373c765c76a6
tees17 <teresa.o.sears@gmail.com>
2015-06-24 23:11:38 -0500

    Unit 2 Multiple Reg

---
+++ multivariate.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+
+loansData = pd.read_csv('LoanStats3c.csv', usecols= ['home_ownership', 'annual_inc', 'int_rate'])
+
+
+#Use income (annual_inc) to model interest rates (int_rate)
+house_ownership = [4 if x == 'OWN' else 3 if x == 'MORTGAGE' else 2 if x == 'RENT' else 1 if x == 'OTHER' else 0 for x in house_ownership]
+own = loansData['home_ownership']
+income=loansData['annual_inc']
+rate=loansData['int_rate']
+income[np.isnan(income)]=0
+rate[np.isnan(rate)]=0
+
+
+
+print loansData.head()
+# The dependent variable
+y = np.matrix(rate).transpose()
+
+#the independent variables
+x1=np.matrix(income).transpose()
+x2=np.matrix(own).transpose()
+
+print x1
+x=np.column_stack([x1,x2])
+X=sm.add_constant(x)
+
+
+model = sm.OLS(y,X)
+f = model.fit()
+print f.summary()
